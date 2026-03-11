6b9cecd08713a2ed5a1dedc22ae16314b399d141
CJJMonty <88670601+CJJMonty@users.noreply.github.com>
2026-03-11 15:59:59 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const SpotifyWebApi = require("spotify-web-api-node");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: process.env.REDIRECT_URI
+});
+
+app.get("/login", (req, res) => {
+  const scopes = ["user-modify-playback-state", "user-read-playback-state"];
+  const authorizeURL = spotifyApi.createAuthorizeURL(scopes);
+  res.redirect(authorizeURL);
+});
+
+app.get("/callback", async (req, res) => {
+  const code = req.query.code;
+
+  try {
+    const data = await spotifyApi.authorizationCodeGrant(code);
+    spotifyApi.setAccessToken(data.body.access_token);
+    spotifyApi.setRefreshToken(data.body.refresh_token);
+    res.send("Spotify connected. You can close this window.");
+  } catch (err) {
+    res.send("Error during authentication");
+  }
+});
+
+app.get("/play", async (req, res) => {
+  try {
+    await spotifyApi.play();
+    res.send("Playing");
+  } catch (err) {
+    res.send("Error");
+  }
+});
+
+app.get("/pause", async (req, res) => {
+  try {
+    await spotifyApi.pause();
+    res.send("Paused");
+  } catch (err) {
+    res.send("Error");
+  }
+});
+
+app.get("/next", async (req, res) => {
+  try {
+    await spotifyApi.skipToNext();
+    res.send("Next track");
+  } catch (err) {
+    res.send("Error");
+  }
+});
+
+app.listen(3000, () => console.log("Server running"));
